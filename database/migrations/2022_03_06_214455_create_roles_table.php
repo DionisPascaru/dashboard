38298903ac272a7113dd00181c55b866e631107c
DionisPascaru <denis.pascaru95@gmail.com>
2022-03-10 23:34:35 +0200

    Update models related between User and Role

--- database/migrations/2022_03_06_214455_create_roles_table.php
+++ database/migrations/2022_03_06_214455_create_roles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateRolesTable extends Migration
@@ -15,9 +16,19 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('role');
-            $table->timestamps();
+            $table->string('role')->unique();
         });
+
+        DB::table('roles')->insert(
+            array(
+                [
+                    'role' => 'Administrator',
+                ],
+                [
+                    'role' => 'Teacher',
+                ],
+            )
+        );
     }
 
     /**
