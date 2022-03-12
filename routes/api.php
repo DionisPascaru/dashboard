d355ea30c9ab3f8b612bfef590bf3ac4d8a6f21d
DionisPascaru <denis.pascaru95@gmail.com>
2022-03-12 23:16:01 +0200

    Create endpoint and controller methods for restfull api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UsersApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
-//Route::middleware('auth:sanctum')->group( function () {
-//    Route::resource('blogs', BlogController::class);
-//});
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('/users', [UsersApiController::class, 'index']);
+
+    Route::group(['prefix' => 'user'], function (){
+        Route::get('/{id}', [UsersApiController::class, 'show']);
+        Route::delete('/{id}', [UsersApiController::class, 'delete']);
+    });
+});
