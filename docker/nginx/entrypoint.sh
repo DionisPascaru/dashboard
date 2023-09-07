7fea72dfd9a45c9e5dfaafae8f3a4199cb740481
dionis <denis.pascaru95@gmail.com>
2023-09-08 00:01:58 +0300

    Config entrypoint.sh

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -2,3 +2,31 @@
 
 envsubst '$NGINX_ROOT $NGINX_FPM_HOST $NGINX_FPM_PORT' < /etc/nginx/fpm.tmpl > /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
+
+if [ ! -z "$WWWUSER" ]; then
+    usermod -u $WWWUSER sail
+fi
+
+if [ ! -d /.composer ]; then
+    mkdir /.composer
+fi
+
+npm install
+composer install
+php artisan migrate
+chmod -R ugo+rw /.composer
+
+if [ ! -z "$PORT" ]; then
+    sed -i -e "s/--port=80/--port=$PORT/g" /etc/supervisor/conf.d/supervisord.conf
+fi
+
+cat .env | grep -q APP_KEY=base64
+if [ ! "$?" -eq 0 ]; then
+    php artisan key:generate
+fi
+
+if [ $# -gt 0 ];then
+    exec gosu $WWWUSER "$@"
+else
+    /usr/bin/supervisord
+fi
