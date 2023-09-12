58f5933fdeb642272ea4ff2f3c911a2deb85ad44
dionis <denis.pascaru95@gmail.com>
2023-09-12 23:16:45 +0300

    Update entrypoint.sh

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -3,30 +3,15 @@
 envsubst '$NGINX_ROOT $NGINX_FPM_HOST $NGINX_FPM_PORT' < /etc/nginx/fpm.tmpl > /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
 
-if [ ! -z "$WWWUSER" ]; then
-    usermod -u $WWWUSER sail
-fi
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
 
-if [ ! -d /.composer ]; then
-    mkdir /.composer
-fi
+echo "Caching config..."
+php artisan config:cache
 
-npm install
-composer install
-php artisan migrate
-chmod -R ugo+rw /.composer
+echo "Caching routes..."
+php artisan route:cache
 
-if [ ! -z "$PORT" ]; then
-    sed -i -e "s/--port=80/--port=$PORT/g" /etc/supervisor/conf.d/supervisord.conf
-fi
-
-cat .env | grep -q APP_KEY=base64
-if [ ! "$?" -eq 0 ]; then
-    php artisan key:generate
-fi
-
-if [ $# -gt 0 ];then
-    exec gosu $WWWUSER "$@"
-else
-    /usr/bin/supervisord
-fi
+echo "Running migrations..."
+php artisan migrate --force
