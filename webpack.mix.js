212b4fc738d9ac2ae02abacbea49286e891a84d4
dionis <denis.pascaru95@gmail.com>
2023-12-27 20:50:38 +0200

    Redesign Sidebar component.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,14 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js').vue();
+mix.js('resources/js/app.js', 'public/js').vue(); // Use .vue() for Vue.js projects
 mix.sass('resources/sass/app.scss', 'public/css');
 mix.postCss('resources/css/app.css', 'public/css');
+
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources'),
+        },
+    },
+});
